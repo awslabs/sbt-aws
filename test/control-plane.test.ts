32dfc672d58dffb489a564767a343f0b946d0379
Toby Buckley <74737385+tobuck-aws@users.noreply.github.com>
2024-03-08 08:17:15 -0800

    feat: Add CloudFormation telemetry (#10)
    * feat: Add CloudFormation telemtry
    
    * Added tests
    
    * assert => expect
    
    * expect => expect().toStrictEqual()

--- test/control-plane.test.ts
+++ test/control-plane.test.ts
@@ -59,7 +59,7 @@ describe('No unsuppressed cdk-nag Warnings or Errors', () => {
   });
 });
 
-describe('ControlPlane', () => {
+describe('ControlPlane without Description', () => {
   const app = new cdk.App();
   interface TestStackProps extends cdk.StackProps {
     systemAdminEmail: string;
@@ -112,5 +112,21 @@ describe('ControlPlane', () => {
     do {
       expect(targetsCapture.asArray()).toHaveLength(1);
     } while (targetsCapture.next());
+  });
+
+  it('should have a fixed template description, when the containing stack does not have description', () => {
+    const actual = controlPlaneTestStack.templateOptions.description;
+    const expected = 'SaaS Builder Toolkit - CoreApplicationPlane (uksb-1tupboc57)';
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should have a concatenated template description, when the containing stack has an existing desc', () => {
+    const stackWithDescription = new TestStack(app, 'stackWithDescription', {
+      systemAdminEmail: 'test@example.com',
+      description: 'ABC',
+    });
+    const actual = stackWithDescription.templateOptions.description;
+    const expected = 'ABC - SaaS Builder Toolkit - CoreApplicationPlane (uksb-1tupboc57)';
+    expect(expected).toStrictEqual(actual);
   });
 });
