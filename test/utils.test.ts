8f6765b14fb62f269de7ef31d74407732be75dbd
Toby Buckley <tobuck@amazon.com>
2024-05-30 14:52:15 -0700

    Tags => tag

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -10,7 +10,7 @@ describe('addTemplateTag', () => {
   it('should append telemetry and tags to existing template description', () => {
     const stackA = new Stack(app, 'TestStackA', { description: 'test' });
     const newTag = 'new-tag';
-    const expectedDescription = `test - ${telemetryConst} (Tags: new-tag)`;
+    const expectedDescription = `test - ${telemetryConst} (tag: new-tag)`;
     addTemplateTag(stackA, newTag);
     expect(stackA.templateOptions.description).toBe(expectedDescription);
   });
@@ -18,25 +18,25 @@ describe('addTemplateTag', () => {
   it('should append a new tag to the description if it does not have any tags', () => {
     const stackB = new Stack(app, 'TestStackB');
     const newTag = 'new-tag';
-    const expectedDescription = `${telemetryConst} (Tags: new-tag)`;
+    const expectedDescription = `${telemetryConst} (tag: new-tag)`;
     addTemplateTag(stackB, newTag);
     expect(stackB.templateOptions.description).toBe(expectedDescription);
   });
 
   it('should append a new tag to the existing tags in the description', () => {
-    const existingDescription = `This is a sample description - ${telemetryConst} (Tags: tag1, tag2)`;
+    const existingDescription = `This is a sample description - ${telemetryConst} (tag: tag1, tag2)`;
     const stackC = new Stack(app, 'TestStackC', { description: existingDescription });
     const newTag = 'new-tag';
-    const expectedDescription = `This is a sample description - ${telemetryConst} (Tags: tag1, tag2, new-tag)`;
+    const expectedDescription = `This is a sample description - ${telemetryConst} (tag: tag1, tag2, new-tag)`;
     addTemplateTag(stackC, newTag);
     expect(stackC.templateOptions.description).toBe(expectedDescription);
   });
 
   it('should not append a new tag if it already exists in the description', () => {
-    const existingDescription = `This is a sample description - ${telemetryConst} (Tags: tag1, tag2, new-tag)`;
+    const existingDescription = `This is a sample description - ${telemetryConst} (tag: tag1, tag2, new-tag)`;
     const stackD = new Stack(app, 'TestStackD', { description: existingDescription });
     const newTag = 'new-tag';
-    const expectedDescription = `This is a sample description - ${telemetryConst} (Tags: tag1, tag2, new-tag)`;
+    const expectedDescription = `This is a sample description - ${telemetryConst} (tag: tag1, tag2, new-tag)`;
     addTemplateTag(stackD, newTag);
     expect(stackD.templateOptions.description).toBe(expectedDescription);
   });
