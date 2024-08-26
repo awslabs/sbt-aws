63e3c24bc34c9fd09904af4dd07109b0a7598d7e
Toby Buckley <tobuck@amazon.com>
2024-08-26 16:18:50 -0700

    Basic docusaurus site

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,101 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'SaaS Builder Toolkit for AWS',
+  tagline: 'A developer toolkit to implement SaaS best practices and increase developer velocity.',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://awslabs.github.io',
+  baseUrl: '/sbt-aws/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'awslabs', // Usually your GitHub org/user name.
+  projectName: 'sbt-aws', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/awslabs/sbt-aws/blob/main/website/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Get Started',
+          items: [
+            {
+              label: 'Docs',
+              to: '/docs/introduction/intro',
+            },
+          ],
+        },
+        {
+          title: 'Get Involved',
+          items: [
+            {
+              label: 'Github',
+              href: 'https://github.com/awslabs/sbt-aws',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} AWS SaaS Factory`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
